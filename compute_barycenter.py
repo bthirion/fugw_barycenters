476da5f3a05bbdbcd84e8da2cbb6288dc4f5bff6
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-04-16 19:00:03 +0200

    iter hyperparams and save

--- compute_barycenter.py
+++ compute_barycenter.py
@@ -1,7 +1,9 @@
+import itertools
 from nilearn import datasets, surface
 import nibabel as nib
 import numpy as np
 from pathlib import Path
+import time
 from tqdm import tqdm
 import torch
 
@@ -12,8 +14,13 @@ from fugw.mappings.barycenter import FUGWBarycenter
 path = Path("/data/parietal/store/data/HCP900/glm/fsaverage5/")
 assert path.exists()
 
-N_SUBJECTS = 10
+N_SUBJECTS = 50
 DEVICE = "cuda"
+# ALPHA_LIST = [0, 0.1, 0.25, 0.5, 0.75, 0.9, 1]
+ALPHA_LIST = [0.5]
+RHO = [1]
+# EPS_LIST = [1e-4, 1e-3, 1e-2, 1e-1]
+EPS_LIST = [1e-3, 1e-2, 1e-1]
 
 # Get the list of subjects
 subjects = [
@@ -73,27 +80,55 @@ geometry_normalized = geometry_normalized.to(DEVICE)
 # Compute fugw barycenter
 d = features_normalized.shape[-1]
 n_subjects = features_normalized.shape[0]
-fugw_bary = FUGWBarycenter()
 weights = torch.ones(d, device=DEVICE) / d
 weights_list = [weights for _ in range(n_subjects)]
 features_list = [features_normalized[i] for i in range(n_subjects)]
+barycenter_path = Path("barycenters")
+barycenter_path.mkdir(exist_ok=True)
 
-barycenter_weights, barycenter_features, barycenter_geometry, _, _, _ = fugw_bary.fit(
-    weights_list=weights_list,
-    features_list=features_list,
-    geometry_list=[geometry_normalized],
-    nits_barycenter=2,
-    solver='mm',
-    solver_params={
-        'nits_bcd': 1,
-        'nits_uot': 3,
-        'eval_bcd': 1,
-        'eval_uot': 1,
-    },
-    device=DEVICE,
-    verbose=True
-)
+print("Total number of combinations: ", len(ALPHA_LIST) * len(RHO) * len(EPS_LIST))
 
-print(f"barycenter_weights.shape: {barycenter_weights.shape}")
-print(f"barycenter_features.shape: {barycenter_features.shape}")
-print(f"barycenter_geometry.shape: {barycenter_geometry.shape}")
+for alpha, rho, eps in itertools.product(ALPHA_LIST, RHO, EPS_LIST):
+    print()
+    start_time = time.time()
+
+    fugw_bary = FUGWBarycenter(
+        alpha=alpha,
+        rho=rho,
+        eps=eps
+    )
+
+    (
+        barycenter_weights,
+        barycenter_features,
+        barycenter_geometry,
+        _,
+        _,
+        _
+    ) = fugw_bary.fit(
+        weights_list=weights_list,
+        features_list=features_list,
+        geometry_list=[geometry_normalized],
+        nits_barycenter=3,
+        solver='mm',
+        solver_params={
+            'nits_bcd': 5,
+            'nits_uot': 50,
+            'eval_bcd': 1,
+            'eval_uot': 1,
+        },
+        device=DEVICE,
+        verbose=False
+    )
+
+    # Save the barycenter
+    barycenter_path_hyp = barycenter_path / f"alpha_{alpha}__rho_{rho}__eps_{eps}"
+    barycenter_path_hyp.mkdir(exist_ok=True)
+    barycenter_weights = barycenter_weights.cpu().numpy()
+    barycenter_features = barycenter_features.cpu().numpy()
+    barycenter_geometry = barycenter_geometry.cpu().numpy()
+    np.save(barycenter_path_hyp / "weights.npy", barycenter_weights)
+    np.save(barycenter_path_hyp / "features.npy", barycenter_features)
+    np.save(barycenter_path_hyp / "geometry.npy", barycenter_geometry)
+
+    print(f"Elapsed time: {time.time() - start_time:.2f} seconds")
